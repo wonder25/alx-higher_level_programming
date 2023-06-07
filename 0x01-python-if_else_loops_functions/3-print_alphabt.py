26b444f2756717d32da9fbfd8165a67dae776868
wonder25 <wonderfulmwinzi14@gmail.com>
2023-06-07 13:49:34 -0700

    prints ascii alphabet without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if (chr(i) == "e" or chr(i) == "q"):
-        continue
-    else:
-        print(chr(i), end="")
+    if chr(i) != 'e' and chr(i) != 'q':
+        print("{}".format(chr(i)), end="")
