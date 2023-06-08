4330340641029cdf915c34226d1ec1f4260f9b95
wonder25 <wonderfulmwinzi14@gmail.com>
2023-06-07 17:22:16 -0700

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 c = 0
 for i in range(26):
-    print("{:s}" .format(chr(122 - i))
+    print("{:s}".format(chr(122 - i))
             if i % 2 == 0
-            else
-                "{:s}".format(chr(90 - i)), end="")
+                else
+                    "{:s}".format(chr(90 - i)), end="")
