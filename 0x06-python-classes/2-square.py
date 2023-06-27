798f2ee60103814cb5a1ccd9f62f71ecc1fe9516
wonder25 <wonderfulmwinzi14@gmail.com>
2023-06-27 15:30:28 -0700

    size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,24 +5,14 @@ This is a module for a class square
 
 
 class Square:
-    """
-    This defines a class square
-
-    Attributes:
-        size (int): Human readable string describing the exception
-    """
+    """This class represents a square"""
 
     def __init__(self, size=0):
         """
-        The __init__ method is documented in the class level 
-        docstring or as a docstring on the __init__ method 
-        itself.
-
-        Both methods are doable but should not be mixed. Choose 
-        one to use to document the __init__ method.
+        Initialize a new square
 
         Args:
-            size (int): Description of `param1`
+            size (int): The size of the new square
         """
 
         if not isinstance(size, int):
@@ -30,6 +20,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >=0")
         self.__size = size
-        """
-        Private instance attribution: size
-        """
