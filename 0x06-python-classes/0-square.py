44ef4fff7927a0c70b9af82ab42fd80fbd786768
wonder25 <wonderfulmwinzi14@gmail.com>
2023-06-27 14:54:56 -0700

    Write a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""This is a module for a class definition of a square"""
+"""
+This is a module for a class definition of a square
+"""
 
 class Square:
-    """This is an implementation of a class square"""
+    """ This is an implementation of a class Square """
     pass
