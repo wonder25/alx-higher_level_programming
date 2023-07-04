16cb54e38321edd6f6449c848be8b367a38ae946
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-04 14:08:26 -0700

    low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" locked class module """
+
+
+class LockedClass:
+    """locked class
+
+    attr:
+        __slots__(private attribute)
+    """
+    __slots__ = ['first_name']
