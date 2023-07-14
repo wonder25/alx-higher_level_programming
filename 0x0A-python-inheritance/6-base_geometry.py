15107e093e31ec03561b10e03df9ccd65ee43923
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-14 01:35:17 -0700

    creates class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Creates class"""
+
+
+class BaseGeometry:
+    """
+    Creates class BaseGeometry:
+        args:
+            def area(self) - computes area
+    """
+
+    def area(self):
+        """Computes area of geometry"""
+
+        raise Exception("area() is not implemented")
