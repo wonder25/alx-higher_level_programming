9162d93cf62c954fa04111343bcbbc62d1cd392f
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-14 02:15:57 -0700

    creates class square that inherits from class Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Creates class Square, child class of Rectangle"""
+"""
+Create class Square, child class of Rectangle
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -10,19 +12,21 @@ class Square(Rectangle):
     Args:
         def __init__(self, size) - Constructor
     """
-
     def __init__(self, size):
-        """Constructor"""
-        
+        """
+        Constructor
+        """
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """Computes area"""
-
+        """
+        computes area
+        """
         return (self.__size * self.__size)
 
     def __str__(self):
-        """Prints friendly format"""
-
+        """
+        Print friendly format
+        """
         return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
