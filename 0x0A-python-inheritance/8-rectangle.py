dc2e387cf2275f9d8cd4621607a8d50e4932e2cf
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-14 01:52:16 -0700

    creates class rectangle that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Creates class Rectangle, child class of BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Create class Rectangle
+
+    Args:
+        def __init__(self, width, height) - Constructor
+    """
+
+    def __init__(self, width, height):
+        """Constructor"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
