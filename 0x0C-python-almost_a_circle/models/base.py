11f2b6a1209e928127cb6a1bdb922653b688845c
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-22 01:39:18 -0700

    created class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Creates class Base"""
+
+
+class Base:
+    """
+    creates class Base
+
+    args:
+        def __init__(self, id=None)
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor"""
+
+        if (id is not None):
+            self.id = id
+        else:
+            # use type as __nb_objects is a private attribute
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
+
