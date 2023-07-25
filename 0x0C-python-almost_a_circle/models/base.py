e831c486eb824186aa9500513f51bc52dd4e8b67
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-25 05:39:37 -0700

    returns JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Creates class Base"""
+import json
 
 
 class Base:
@@ -20,3 +21,10 @@ class Base:
             # use type as __nb_objects is a private attribute
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Converts dictionary to JSON repr"""
+        if (list_dictionaries == [] or list_dictionaries is None):
+            return "[]"
+        return json.dumps(list_dictionaries)
