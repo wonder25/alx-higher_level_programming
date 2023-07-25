6c65177ae3e367a21d3a2bb29a29d64ab9802346
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-25 03:47:28 -0700

    string repr

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,6 +114,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """String represantation"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y}\ - {self.width}/{self.height}"
-
-
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
