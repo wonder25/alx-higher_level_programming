e8463721dbeea2c565d35c58520418e97dd1cdd7
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-25 01:56:52 -0700

    creates class rectangle that inherits from base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,22 @@ class Rectangle(Base):
 
     Args:
         def __init__(self, width, height, x=0, y=0, id=None)
+        @property
+        def width(self)
+        @width.setter
+        def set_width(self, value)
+        @property
+        def height(self)
+        @height.setter
+        def set_height(self, value)
+        @property
+        def x(self)
+        @x.setter
+        def set_x(self, value)
+        @property
+        def y(self)
+        @y.setter
+        def set_y(self, value)
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -20,3 +36,39 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """Gets width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """Gets x value"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """Gets y value"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
