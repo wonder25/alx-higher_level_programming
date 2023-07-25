ecfdf5dbbdbdb415093a9cfee1caaadd1675bfd7
wonder25 <wonderfulmwinzi14@gmail.com>
2023-07-25 04:09:09 -0700

    assigns a key/value argument to attributes:

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,7 +117,7 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y}\
  - {self.width}/{self.height}"
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update instance attributes"""
         if (len(args) > 0):
             try:
