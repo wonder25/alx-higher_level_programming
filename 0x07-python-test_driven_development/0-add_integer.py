145fed0eb57d383b329d333b0f154351c9022de2
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-04 02:31:56 -0700

    function add integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Function that adss two integers"""
+
+
+def add_integer(a, b=98):
+    """Addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value (a+b)
+    """
+    if (not isinstance(a, (int, float))):
+        raise TypeError("a must be an integer")
+
+    if (not isinstance(b, (int, float))):
+        raise TypeError("b must be an integer")
+
+    if (isinstance(a, float)):
+        a = int(a)
+
+    if (isinstance(b, float)):
+        b = int(b)
+    return (a + b)
+
