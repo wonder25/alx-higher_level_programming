668e0ca55ab374ce59ee235514cc98d39ef067de
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-13 14:12:22 -0700

    writes a string to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" wites a string to a text file """
+""" writes a string to a text file """
 
 
 def write_file(filename="", text=""):
     """ writes a string to a file and returns the number of 
     characters written.
     """
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, 'w', encoding="utf-8") as file:
         return file.write(text)
