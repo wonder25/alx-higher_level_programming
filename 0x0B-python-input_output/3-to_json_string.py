c0530eb1813a6cb46a1274549b475b7d02eaee1c
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-13 14:49:17 -0700

    fix task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-""" Jsonify string """
-
+"""
+Jsonify string
+"""
 import json
 
 
 def to_json_string(my_obj):
-    
-    """ returns JSON rep of an object(string) """
-    return json.dumps(my_obj)
+     """
+     returns JSON rep of an object(string)
+     """
+     return json.dumps(my_obj)
