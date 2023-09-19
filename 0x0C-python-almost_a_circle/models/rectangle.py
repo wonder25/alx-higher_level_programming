0460bd4f2833facb7cc877c008a04bc396c7af03
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-19 02:38:52 -0700

    overrides the string method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,5 +82,5 @@ class Rectangle(Base):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
-        """ overides the __str__ method """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x , self.y, self.width, self.height)
+        """ overrides the __str__ method """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
