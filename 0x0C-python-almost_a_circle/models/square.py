3df0a7c7aecccc5839484f531e791c8e62ed6d21
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-19 05:57:04 -0700

    returns the dictionary representation of a rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,39 @@ class Square(Rectangle):
         else:
             self.width = value
             self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        *args is the list of arguments - number of keyworded arguments
+        **kwargs can be thought of as a double pointer to a dictionary
+        key/value (keyworded arguments)
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[2]
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'size':
+                    self.width = value
+                if key == 'x':
+                    self.x = value
+                if key == 'y':
+                    self.y = value
+
+    def to_dictionary(self):
+        """
+        returns the dictionary representation of a Rectangle
+        """
+        dic = {}
+        dic['x'] = self.x
+        dic['y'] = self.y
+        dic['id'] = self.id
+        dic['size'] = self.width
+        return dic
