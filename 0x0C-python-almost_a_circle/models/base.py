16e42b3381337467685eb73dcab025e6c7300753
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-19 06:09:45 -0700

    updated base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,3 +15,46 @@ class Base:
         else:
             __class__.__nb_objects += 1
             self.id = __class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns json string representation of list_dictionaries
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return '[]'
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the json string representation list object to a file
+        """
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        obj_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_str = cls.to_json_string(obj_dicts)
+        with open(filename, mode='w', encoding='utf-8') as file:
+            file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the json string representation
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributer already set
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
