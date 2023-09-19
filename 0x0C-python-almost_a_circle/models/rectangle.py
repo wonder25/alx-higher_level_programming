e5487e5b98f0e93c3d8e8b96879f288cbc44647a
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-19 02:13:43 -0700

    rectangle class inherits from base

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" class rectangle """
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ a subclass of Base """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ initializes the class """
+        self.width = width
+        self.height = height
+        self.y = y
+        self.x = x
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ getting the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setting the settter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getting the height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setting the setter """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ getting the x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ setting the setter """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ getting the y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ setting the setter """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
