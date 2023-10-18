82794cf65b5a4e4218a1abd2f9efe7a85197c629
wonder25 <wonderfulmwinzi14@gmail.com>
2023-10-18 11:33:25 +0300

    creates another table and add multiples rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creates a table and adds multiple rows
+CREATE table IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table VALUES (1, "John", 10);
+INSERT INTO second_table VALUES (2, "Alex", 3);
+INSERT INTO second_table VALUES (3, "Bob", 14);
+INSERT INTO second_table VALUES (4, "George", 8);
