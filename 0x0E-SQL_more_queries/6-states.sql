801fc6ec065d0c85ad344b5b9155ae8c1f08070d
wonder25 <wonderfulmwinzi14@gmail.com>
2023-10-20 05:07:25 +0300

    states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL, name VARCHAR(256) NOT NULL);
