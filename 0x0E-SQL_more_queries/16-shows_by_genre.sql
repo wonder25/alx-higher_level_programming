4afe8bc1f4a40208d9a026b0840593c4f3d7b348
wonder25 <wonderfulmwinzi14@gmail.com>
2023-10-22 11:49:13 +0300

    lists all shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,5 +4,5 @@ FROM tv_shows
 LEFT JOIN tv_shows_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id;
+ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
