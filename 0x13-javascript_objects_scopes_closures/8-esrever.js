83763f9d71d502e96f6bd32f6b0776a3195bf394
wonder25 <wonderfulmwinzi14@gmail.com>
2023-12-01 14:43:49 +0300

    update

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  const revList = [];
-  list.forEach(element => {
-    revList.unshift(element);
-  });
-  return revList;
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-let argCnt = 0;
-
-exports.logMe = function (item) {
-  console.log(`${argCnt}: ${item}`);
-  argCnt++;
-};
