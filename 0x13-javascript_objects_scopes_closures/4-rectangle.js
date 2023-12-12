3f69d91f6eeb03e149ebd6449af134dc1a4ccb3a
wonder25 <wonderfulmwinzi14@gmail.com>
2023-12-13 00:32:57 +0300

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(`${'X'.repeat(this.width)}`);
+    }
+  }
+
+  rotate () {
+    const tmpWidth = this.width;
+  
+    this.width = this.height;
+    this.height = tmpWidth;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+module.exports = Rectangle;
+
