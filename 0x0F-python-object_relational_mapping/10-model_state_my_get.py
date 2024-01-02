b2519f5ab11bf7ae7dfb87abecbcefff773baa1c
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-02 09:07:53 -0800

    Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""List all State objects with letter a"""
+
+if __name__ == '__main__':
+
+    import sqlalchemy
+    from sqlalchemy import create_engine,\
+        Column, Integer, UniqueConstraint, String
+    from sqlalchemy.ext.declarative import declarative_base
+    from model_state import Base, State
+    import sys
+    from sqlalchemy.orm import sessionmaker
+
+    USR = sys.argv[1]
+    PWD = sys.argv[2]
+    DB = sys.argv[3]
+    HST = 'localhost'
+    PRT = '3306'
+    state_name = sys.argv[4]
+
+    connection_url = f"mysql://{USR}:{PWD}@{HST}:{PRT}/{DB}"
+
+    """connect to DB"""
+    engine = create_engine(connection_url)
+
+    """Session class for session objects"""
+    Session = sessionmaker(bind=engine)
+
+    """session with database started"""
+    session = Session()
+
+    """sample query"""
+    states = session.query(State).\
+        filter(State.name == state_name).order_by(State.id.asc()).all()
+
+    """Iterating through results of query"""
+    if (len(states) != 0):
+        for state in states:
+            print(f"{state.id}")
+    else:
+        print("Not found")
