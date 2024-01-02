ffd5d1131153507df9cd76e1b586451d6946dcc2
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-02 09:46:36 -0800

    delete states

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Delete states with 'a'"""
+
+if __name__ == '__main__':
+
+    import sqlalchemy
+    from sqlalchemy import create_engine,\
+        Column, Integer, UniqueConstraint, String
+    from sqlalchemy.ext.declarative import declarative_base
+    from model_state import Base, State
+    import sys
+    from sqlalchemy.orm import sessionmaker
+
+    USR = sys.argv[1]
+    PWD = sys.argv[2]
+    DB = sys.argv[3]
+    HST = 'localhost'
+    PRT = '3306'
+
+    connection_url = f"mysql://{USR}:{PWD}@{HST}:{PRT}/{DB}"
+
+    """Engine to connect to DB"""
+    engine = create_engine(connection_url)
+
+    """Session class"""
+    Session = sessionmaker(bind=engine)
+
+    """database started"""
+    session = Session()
+
+    """Sample query"""
+    states = session.query(State).\
+        filter(State.name.like('%a%')).order_by(State.id.asc())
+
+    for state in states:
+        session.delete(state)
+
+    """Commit changes"""
+    session.commit()
