6264131bcb72f3ccad950bc775789a8271d1933b
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-14 13:29:57 +0300

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,4 +5,4 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    prints(requests.post(argv[1], data={'email': argv[2]}).text)
+    print(requests.post(argv[1], data={'email': argv[2]}).text)
