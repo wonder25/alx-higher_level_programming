97c2c074c0e613ff42fe8438da6ed3a2e92b8bb6
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-14 11:29:23 +0300

    fetches url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python script that fetches a url"""
+
+from urllib import request
+
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
