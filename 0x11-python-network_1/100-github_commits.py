6b21494f7468bd31c3bcd354f5f69fe381fe0b1b
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-14 14:01:43 +0300

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 """lists 10 commits from a user"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    total = 0
+    commit_str = "{}: {}"
+    url = "https://api.github.com/repos/{}/{}/commits"
+    formated_url = url.format(argv[2], argv[1])
+    for commit in requests.get(formated_url).json():
+        if total < 10:
+            sha = commit.get("sha")
+            name = commit.get("commit").get("author").get("name")
+            print(commit_str.format(sha, name))
+        total += 1
