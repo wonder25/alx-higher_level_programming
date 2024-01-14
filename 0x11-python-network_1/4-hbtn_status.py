5938074c4332c354064f92e4790065fa1a2be4c5
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-14 13:11:23 +0300

    task 4 fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     text = requests.get(url).text
-    rtr_str = 'Body Response:\n\t- type: {}\n\t- content: {}'
+    rtr_str = 'Body response:\n\t- type: {}\n\t- content: {}'
     print(rtr_str.format(type(text), text))
